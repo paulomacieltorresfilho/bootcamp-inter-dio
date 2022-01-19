4f8d060bb5138ec9bc86e7be733e84474c27a2dd
Paulo Maciel <paulomacieltorresfilho@gmail.com>
2022-01-19 18:23:56 -0300

    desafio de codigo manipulando DOM quase finalizado

--- Cursos Javascript/Manipulando a DOM com Javascript/Código/assets/script.js
+++ Cursos Javascript/Manipulando a DOM com Javascript/Código/assets/script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const botao = document.getElementById('botao')
+const elements = document.querySelectorAll('.mode');
+
+botao.addEventListener('click', () => {
+    elements.forEach((item) => {
+        item.classList.toggle('dark-mode');
+
+        if (item.tagName === 'BUTTON') {
+            if (item.innerHTML === 'Light Mode'){
+                item.innerHTML = 'Dark Mode';
+            } else {item.innerHTML = 'Light Mode'};
+        }
+
+    })
+})
