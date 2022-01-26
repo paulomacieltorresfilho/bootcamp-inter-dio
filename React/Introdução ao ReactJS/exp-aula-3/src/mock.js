5364330186f280c0ebaf71a7b9ee062598037101
Paulo Maciel <paulomacieltorresfilho@gmail.com>
2022-01-26 13:33:23 -0300

    finalizado curso de introducao a React

---
+++ React/Introdução ao ReactJS/exp-aula-3/src/mock.js
@@ -0,0 +1,29 @@
+
+
+const mock = {
+
+    listCustomers: [
+        {
+            id:1,
+            name:'Paulo Maciel'
+        },
+        {
+            id: 2,
+            name:'Maria Clara'
+        },
+        {
+            id: 3,
+            name: 'Pedro Essucy'
+        },
+        {
+            id: 4,
+            name: 'Carol Awada'
+        },
+        {
+            id: 5,
+            name: 'João Gabriel'
+        }
+    ]
+}
+
+export default mock;
